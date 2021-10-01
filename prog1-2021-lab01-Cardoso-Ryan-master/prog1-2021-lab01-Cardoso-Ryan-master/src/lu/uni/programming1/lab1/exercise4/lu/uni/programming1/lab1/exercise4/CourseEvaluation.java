a951e2e0332b7c8f27d7fd29345b5d45558e01d8
Ryan CARDOSO <Ryan.Cardoso.001@student.uni.lu>
2021-10-01 08:44:59 +0200

    exercice4 updated

---
+++ prog1-2021-lab01-Cardoso-Ryan-master/prog1-2021-lab01-Cardoso-Ryan-master/src/lu/uni/programming1/lab1/exercise4/lu/uni/programming1/lab1/exercise4/CourseEvaluation.java
@@ -0,0 +1,20 @@
+package lu.uni.programming1.lab1.exercise4;
+
+import java.util.Scanner;
+public class CourseEvaluation {
+
+    public static void main(String args[]) 
+    {
+        double mt1, mt2, fe, average;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Midterm 1 : ");
+        mt1 = Math.max(0, Math.min(20, scan.nextDouble()));
+        System.out.print("Enter Midterm 2 : ");
+        mt2 = Math.max(0, Math.min(20, scan.nextDouble()));
+        System.out.print("Enter Final Exam : ");
+        fe = Math.max(0, Math.min(20, scan.nextDouble()));
+        average = mt1*0.2 + mt2*0.2 + fe*0.6 ; 
+        System.out.print("The average is: " +average);
+        scan.close();
+    }
+}
