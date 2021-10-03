8c3ab2e5bec9d67969e5176321e79bc75c95d5c0
Ryan CARDOSO <Ryan.Cardoso.001@student.uni.lu>
2021-10-03 20:54:46 +0200

    exercise7

---
+++ prog1-2021-lab01-Cardoso-Ryan-master/prog1-2021-lab01-Cardoso-Ryan-master/src/lu/uni/programming1/lab1/exercise7/Swapping.java
@@ -0,0 +1,16 @@
+
+public class Swapping {
+ 
+    public static void main(String[] args){
+    int a, b;
+    a = 24;
+    b = 11;
+    System.out.println("Before swapping : a is : "+a+" ,and b is :"+b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    System.out.println("After swapping : a is : "+a+" ,and b is :"+b);
+    
+    }
+
+}
